4f029933b41987fb329103340da0a160d41cd848
olga_demina <olga.demina1410@gmail.com>
2026-01-20 12:13:28 +0100

    feat: images addes

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from "express";
 import booksRouter from "./routers/books.js";
 import handleError from "./middlewares/handleError.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.SERVER_PORT;
+
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use("/api/books", booksRouter);
 
